f4abd9cc696c80875d644909b189ee31f4066fe7
Usman Arshad <>
2026-01-04 10:15:55 +0500

    downloading and populating productList

---
+++ js/index.js
@@ -0,0 +1,23 @@
+async function fetchCategories(){
+    const response = await fetch('https://fakestoreapi.com/products/categories');
+    const data= await response.json();
+    return data;
+}
+
+
+async function populateCategories(){
+    const categories = await fetchCategories();
+    console.log("categories",categories)
+    const categoryList=document.getElementById("cateogryList");
+    categories.forEach((category)=>{
+        const categoryHolder=document.createElement("div");
+        const categoryLink=document.createElement("a");
+        categoryLink.href="#";
+        categoryLink.textContent=category;
+        categoryHolder.classList.add("cateogry-item","d-flex" ,"align-items-center" ,"justify-content-center");
+        categoryHolder.appendChild(categoryLink);
+        categoryList.appendChild(categoryHolder);
+    })
+}
+
+populateCategories();
